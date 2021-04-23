981b1bbb0a0ccad7826ff40f4737b73fe3086d94
TheSwerik <derswerik@gmail.com>
2021-04-23 15:48:02 +0200

    change publish comamnd

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,1 @@
-﻿dotnet publish -p:PublishProfile=InjectHelloWorldConsole/InjectHelloWorldConsole.csproj -c:Release -o:Publish\bin\64bit -v:m -r:win-x64 --self-contained:true -p:PublishSingleFile=true -p:PublishTrimmed=true
+﻿dotnet publish -p:PublishProfile=InjectHelloWorldConsole/InjectHelloWorldConsole.csproj -c:Release -o:Publish\bin\64bit -v:m -r:win-x64 --self-contained:false -p:PublishSingleFile=false
